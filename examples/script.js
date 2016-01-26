be92374c146ae2327fad15e1a60509ed0b7d77ed
Christian Ulbrich (Zalari UG haftungsbeschränkt) <christian.ulbrich@zalari.de>
2016-01-26 14:31:11 +0100

    fix scripts for example

--- examples/script.js
+++ examples/script.js
@@ -1,15 +1,15 @@
-function testController($scope) {
+'use strict';
+angular.module('pickadateExample', [])
+  .controller('testController', function ($scope) {
     $scope.curDate = '';
-    $scope.newDate = function() {
-        return new Date();
+    $scope.newDate = function () {
+      return new Date();
     };
-}
-
-function testController2($scope) {
+  })
+  .controller('testController2', function ($scope) {
     $scope.startDate = '2014-02-24 12:00:00';
     $scope.endDate = '2014-02-27 12:00:00';
-}
-
-function testController3($scope) {
+  })
+  .controller('testController3', function ($scope) {
     $scope.curDate = '2014-02-24 12:00:00';
-}
+  });
